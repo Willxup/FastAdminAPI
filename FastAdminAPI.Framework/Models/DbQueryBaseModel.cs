32a64f1b4dea3f77b81c06af14804ea860829f79
Wang Xin <wangxking@outlook.com>
2024-04-09 19:18:23 +0800

    fix query bug and update nuget package

--- FastAdminAPI.Framework/Models/DbQueryBaseModel.cs
+++ FastAdminAPI.Framework/Models/DbQueryBaseModel.cs
@@ -1,18 +1,23 @@
-﻿namespace FastAdminAPI.Framework.Extensions.Models
+﻿using SqlSugar.Attributes.Extension.Extensions;
+
+namespace FastAdminAPI.Framework.Extensions.Models
 {
     public class DbQueryBaseModel
     {
         /// <summary>
         /// 第几页（从1开始）
         /// </summary>
+        [DbIgnoreField]
         public int? Index { get; set; } = null;
         /// <summary>
         /// 返回的每页行数
         /// </summary>
+        [DbIgnoreField]
         public int? Size { get; set; } = null;
         /// <summary>
         /// 偏移
         /// </summary>
+        [DbIgnoreField]
         public virtual int? Offset => (Index - 1) * Size;
     }
 }
