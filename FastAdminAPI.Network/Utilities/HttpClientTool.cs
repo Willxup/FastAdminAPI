1d79aaddcfde34a8db3970d6e5dc5c66bea92480
Willxup <wangxking@outlook.com>
2024-04-26 13:42:50 +0800

    feat:add comments of httpclient tool

--- FastAdminAPI.Network/Utilities/HttpClientTool.cs
+++ FastAdminAPI.Network/Utilities/HttpClientTool.cs
@@ -29,72 +29,87 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// 构建HttpClient
         /// </summary>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        private HttpClient BuildHttpClient(Dictionary<string, string> dicDefaultHeaders, int? timeoutSecond)
+        private HttpClient BuildHttpClient(Dictionary<string, string> headers, int? timeoutSecond)
         {
             HttpClient httpClient = _httpClientFactory.CreateClient();
 
+            // 清空默认请求头
             httpClient.DefaultRequestHeaders.Clear();
+
+            // 设置content-type
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // 设置编码格式
             httpClient.DefaultRequestHeaders.AcceptCharset.Add(new StringWithQualityHeaderValue("UTF-8"));
 
-            if (dicDefaultHeaders != null && dicDefaultHeaders?.Count > 0)
+            // 设置自定义请求头
+            if (headers != null && headers?.Count > 0)
             {
-                foreach (var headerItem in dicDefaultHeaders)
+                foreach (var item in headers)
                 {
-                    if (!httpClient.DefaultRequestHeaders.Contains(headerItem.Key))
+                    if (!httpClient.DefaultRequestHeaders.Contains(item.Key))
                     {
-                        httpClient.DefaultRequestHeaders.Add(headerItem.Key, headerItem.Value);
+                        httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
                     }
                 }
             }
+
+            // 设置超时
             if (timeoutSecond != null && timeoutSecond > 0)
             {
                 httpClient.Timeout = TimeSpan.FromSeconds(timeoutSecond.Value);
             }
+
             return httpClient;
         }
         /// <summary>
         /// 生成HttpRequestMessage
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="requestBody"></param>
-        /// <param name="method"></param>
-        /// <param name="dicHeaders"></param>
+        /// <param name="url">url</param>
+        /// <param name="requestBody">请求体</param>
+        /// <param name="method">http请求方式</param>
+        /// <param name="headers">请求头</param>
         /// <returns></returns>
-        private static HttpRequestMessage GenerateHttpRequestMessage(string url, string requestBody, HttpMethod method, Dictionary<string, string> dicHeaders)
+        private static HttpRequestMessage GenerateHttpRequestMessage(string url, string requestBody, HttpMethod method, Dictionary<string, string> headers)
         {
             var request = new HttpRequestMessage(method, url);
+            
             if (!string.IsNullOrEmpty(requestBody))
             {
                 request.Content = new StringContent(requestBody);
             }
-            if (dicHeaders != null && dicHeaders?.Count > 0)
+
+            if (headers != null && headers?.Count > 0)
             {
-                foreach (var header in dicHeaders)
+                foreach (var item in headers)
                 {
-                    request.Headers.Add(header.Key, header.Value);
+                    request.Headers.Add(item.Key, item.Value);
                 }
             }
+
             return request;
         }
         /// <summary>
         ///  生成请求体(包含请求头)
         /// </summary>
-        /// <param name="requestBody"></param>
-        /// <param name="dicHeaders"></param>
+        /// <param name="requestBody">请求体</param>
+        /// <param name="headers">请求头</param>
         /// <returns></returns>
-        private static StringContent GenerateStringContent(string requestBody, Dictionary<string, string> dicHeaders)
+        private static StringContent GenerateStringContent(string requestBody, Dictionary<string, string> headers)
         {
             var content = new StringContent(requestBody);
-            if (dicHeaders != null && dicHeaders?.Count > 0)
+
+            if (headers != null && headers?.Count > 0)
             {
-                foreach (var headerItem in dicHeaders)
+                foreach (var item in headers)
                 {
-                    content.Headers.Add(headerItem.Key, headerItem.Value);
+                    content.Headers.Add(item.Key, item.Value);
                 }
             }
+
             return content;
         }
         /// <summary>
@@ -107,17 +122,17 @@ namespace FastAdminAPI.Network.Utilities
         {
             try
             {
-                //空校验
+                // 空校验
                 if (string.IsNullOrEmpty(responseContent))
                 {
                     return default;
                 }
-                //返回结果类型为字符串
+                // 返回结果类型为字符串
                 if (typeof(TResult) == typeof(string))
                 {
                     return (TResult)Convert.ChangeType(responseContent, typeof(TResult));
                 }
-                //其他
+                // 其他
                 else
                 {
                     var result = JsonConvert.DeserializeObject<TResult>(responseContent);
@@ -134,27 +149,30 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// 获取HTTPClient
         /// </summary>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public HttpClient GetHttpClient(Dictionary<string, string> dicHeaders = null, int timeoutSecond = 60)
+        public HttpClient GetHttpClient(Dictionary<string, string> headers = null, int timeoutSecond = 60)
         {
-            return BuildHttpClient(dicHeaders, timeoutSecond);
+            return BuildHttpClient(headers, timeoutSecond);
         }
         /// <summary>
         /// Get
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="url">url</param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public async Task<T> GetAsync<T>(string url, Dictionary<string, string> dicHeaders = null, int timeoutSecond = 60)
+        public async Task<T> GetAsync<T>(string url, Dictionary<string, string> headers = null, int timeoutSecond = 60)
         {
             try
             {
-                var client = BuildHttpClient(dicHeaders, timeoutSecond);
+                var client = BuildHttpClient(headers, timeoutSecond);
+
                 var response = await client.GetAsync(url);
+
                 var responseContent = await response.Content.ReadAsStringAsync();
+
                 if (response.IsSuccessStatusCode)
                 {
                     return ConvertResponseContentToResult<T>(responseContent);
@@ -173,19 +191,23 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// Post
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="requestBody"></param>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="url">url</param>
+        /// <param name="requestBody">请求体</param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public async Task<T> PostAsync<T>(string url, string requestBody, Dictionary<string, string> dicHeaders = null, int timeoutSecond = 60)
+        public async Task<T> PostAsync<T>(string url, string requestBody, Dictionary<string, string> headers = null, int timeoutSecond = 60)
         {
             try
             {
                 var client = BuildHttpClient(null, timeoutSecond);
-                var requestContent = GenerateStringContent(requestBody, dicHeaders);
+
+                var requestContent = GenerateStringContent(requestBody, headers);
+
                 var response = await client.PostAsync(url, requestContent);
+
                 var responseContent = await response.Content.ReadAsStringAsync();
+
                 if (response.IsSuccessStatusCode)
                 {
                     return ConvertResponseContentToResult<T>(responseContent);
@@ -204,19 +226,23 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// Put
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="requestBody"></param>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="url">url</param>
+        /// <param name="requestBody">请求体</param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public async Task<T> PutAsync<T>(string url, string requestBody, Dictionary<string, string> dicHeaders = null, int timeoutSecond = 60)
+        public async Task<T> PutAsync<T>(string url, string requestBody, Dictionary<string, string> headers = null, int timeoutSecond = 60)
         {
             try
             {
                 var client = BuildHttpClient(null, timeoutSecond);
-                var requestContent = GenerateStringContent(requestBody, dicHeaders);
+
+                var requestContent = GenerateStringContent(requestBody, headers);
+
                 var response = await client.PutAsync(url, requestContent);
+
                 var responseContent = await response.Content.ReadAsStringAsync();
+
                 if (response.IsSuccessStatusCode)
                 {
                     return ConvertResponseContentToResult<T>(responseContent);
@@ -236,18 +262,23 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// Patch
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="url">url</param>
+        /// <param name="requestBody">请求体</param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public async Task<T> PatchAsync<T>(string url, string requestBody, Dictionary<string, string> dicHeaders = null, int timeoutSecond = 60)
+        public async Task<T> PatchAsync<T>(string url, string requestBody, Dictionary<string, string> headers = null, int timeoutSecond = 60)
         {
             try
             {
                 var client = BuildHttpClient(null, timeoutSecond);
-                var requestContent = GenerateStringContent(requestBody, dicHeaders);
+
+                var requestContent = GenerateStringContent(requestBody, headers);
+
                 var response = await client.PatchAsync(url, requestContent);
+
                 var responseContent = await response.Content.ReadAsStringAsync();
+
                 if (response.IsSuccessStatusCode)
                 {
                     return ConvertResponseContentToResult<T>(responseContent);
@@ -266,17 +297,20 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// Delete
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="url">url</param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public async Task<T> DeleteAsync<T>(string url, Dictionary<string, string> dicHeaders = null, int timeoutSecond = 60)
+        public async Task<T> DeleteAsync<T>(string url, Dictionary<string, string> headers = null, int timeoutSecond = 60)
         {
             try
             {
-                var client = BuildHttpClient(dicHeaders, timeoutSecond);
+                var client = BuildHttpClient(headers, timeoutSecond);
+
                 var response = await client.DeleteAsync(url);
+
                 var responseContent = await response.Content.ReadAsStringAsync();
+
                 if (response.IsSuccessStatusCode)
                 {
                     return ConvertResponseContentToResult<T>(responseContent);
@@ -295,20 +329,24 @@ namespace FastAdminAPI.Network.Utilities
         /// <summary>
         /// common request
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="method"></param>
-        /// <param name="requestBody"></param>
-        /// <param name="dicHeaders"></param>
-        /// <param name="timeoutSecond"></param>
+        /// <param name="url">url</param>
+        /// <param name="method">http请求方式</param>
+        /// <param name="requestBody">请求体</param>
+        /// <param name="headers">请求头</param>
+        /// <param name="timeoutSecond">超时时间</param>
         /// <returns></returns>
-        public async Task<T> ExecuteAsync<T>(string url, HttpMethod method, string requestBody, Dictionary<string, string> dicHeaders, int timeoutSecond = 60)
+        public async Task<T> ExecuteAsync<T>(string url, HttpMethod method, string requestBody, Dictionary<string, string> headers, int timeoutSecond = 60)
         {
             try
             {
                 var client = BuildHttpClient(null, timeoutSecond);
-                var request = GenerateHttpRequestMessage(url, requestBody, method, dicHeaders);
+
+                var request = GenerateHttpRequestMessage(url, requestBody, method, headers);
+
                 var response = await client.SendAsync(request);
+
                 var responseContent = await response.Content.ReadAsStringAsync();
+
                 if (response.IsSuccessStatusCode)
                 {
                     return ConvertResponseContentToResult<T>(responseContent);
