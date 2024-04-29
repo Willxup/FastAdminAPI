ca03dd03e6eabdf82716bef144bca44e83ecac41
Willxup <wangxking@outlook.com>
2024-04-29 17:17:34 +0800

    modify qrcode tool

--- FastAdminAPI.Common/QRCode/QRCodeTool.cs
+++ FastAdminAPI.Common/QRCode/QRCodeTool.cs
@@ -2,14 +2,14 @@
 
 namespace FastAdminAPI.Common.QRCode
 {
-    public static class QRCodeHelper
+    public static class QRCodeTool
     {
         /// <summary>
         /// 生成二维码图片
         /// </summary>
         /// <param name="content">二维码内容</param>
         /// <param name="pixel">像素大小</param>
-        /// <returns></returns>
+        /// <returns>二维码图片</returns>
         public static byte[] GetQRCode(string content, int pixel)
         {
             QRCodeData qrCodeData = QRCodeGenerator.GenerateQrCode(content, QRCodeGenerator.ECCLevel.M);
