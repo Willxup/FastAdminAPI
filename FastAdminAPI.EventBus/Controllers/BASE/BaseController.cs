7c54e2cb3077f98144ff961408e1d9bb0eb23749
Willxup <wangxking@outlook.com>
2024-05-16 16:10:08 +0800

    add service to service api

---
+++ FastAdminAPI.EventBus/Controllers/BASE/BaseController.cs
@@ -0,0 +1,50 @@
+﻿using FastAdminAPI.Common.BASE;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastAdminAPI.EventBus.Controllers.BASE
+{
+    /// <summary>
+    /// 控制器基类
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+
+        /// <summary>
+        /// 通用响应-成功
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        protected ResponseModel Success(object data = null, string msg = null)
+        {
+            ResponseModel result = ResponseModel.Success(data);
+
+            if (!string.IsNullOrEmpty(msg)) result.Message = msg;
+
+            return result;
+        }
+        /// <summary>
+        /// 通用响应-错误
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        protected ResponseModel Error(string msg, ResponseCode code = ResponseCode.Error)
+        {
+            return ResponseModel.Error(msg, code);
+        }
+        /// <summary>
+        /// 通用响应-警告
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="data"></param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        protected ResponseModel Warn(string msg, object data = null, ResponseCode code = ResponseCode.Warn)
+        {
+            return ResponseModel.Warn(msg, code, data);
+        }
+    }
+}
---
+++ FastAdminAPI.EventBus/Controllers/CrossApiController.cs
@@ -0,0 +1,70 @@
+﻿using FastAdminAPI.Common.BASE;
+using FastAdminAPI.EventBus.Controllers.BASE;
+using FastAdminAPI.Network.Interfaces;
+using FastAdminAPI.Network.Models.Wechat;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace FastAdminAPI.EventBus.Controllers
+{
+    /// <summary>
+    /// 跨服务API
+    /// </summary>
+    public class CrossApiController : BaseController
+    {
+        /// <summary>
+        /// 邮件API
+        /// </summary>
+        private readonly IEmailApi _emailApi;
+        /// <summary>
+        /// 微信API
+        /// </summary>
+        private readonly IWechatApi _wechatApi;
+
+        /// <summary>
+        /// 跨服务API
+        /// </summary>
+        public CrossApiController(IEmailApi emailApi, IWechatApi wechatApi)
+        {
+            _emailApi = emailApi;
+            _wechatApi = wechatApi;
+        }
+
+        /// <summary>
+        /// 获取微信接口调用权限签名
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(WeChatSignModel), 200)]
+        public async Task<ResponseModel> GetWeChatAccessToken([FromBody] WeChatSignRequestModel model)
+        {
+            return await _wechatApi.GetWechatSign(model);
+        }
+        /// <summary>
+        /// 获取微信公众号用户OpenId
+        /// </summary>
+        /// <param name="appId">微信公众号AppId</param>
+        /// <param name="code">授权code</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ResponseModel> GetWechatUserOpenId([FromQuery][Required(ErrorMessage = "微信公众号AppId不能为空!")] string appId,
+            [FromQuery][Required(ErrorMessage = "授权code不能为空!")] string code)
+        {
+            return await _wechatApi.GetWechatUserOpenId(appId, code);
+        }
+        /// <summary>
+        /// 发送Smtp邮件
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="body">内容</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ResponseModel> SendEmailByDefault([FromQuery][Required(ErrorMessage = "标题不能为空!")] string title, [FromQuery] string body)
+        {
+            await _emailApi.SendEmailByDefault(title, body);
+            return Success();
+        }
+    }
+}
