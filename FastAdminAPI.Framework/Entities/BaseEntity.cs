ccb4b016f9b1db64fe8c06c8a12e336b4a8659da
Willxup <wangxking@outlook.com>
2024-07-09 17:41:32 +0800

    delete unusing reference

--- FastAdminAPI.Framework/Entities/BaseEntity.cs
+++ FastAdminAPI.Framework/Entities/BaseEntity.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FastAdminAPI.Framework.Entities
 {
--- FastAdminAPI.Framework/Entities/S98_RegionInfo.cs
+++ FastAdminAPI.Framework/Entities/S98_RegionInfo.cs
@@ -1,13 +1,11 @@
 ﻿
-using System;
-using System.Linq;
-using System.Text;
 using SqlSugar;
+using System;
 
 
 namespace FastAdminAPI.Framework.Entities
 {
-        ///<summary>
+    ///<summary>
     /// 区域
     ///</summary>
     [SugarTable("S98_RegionInfo")]
