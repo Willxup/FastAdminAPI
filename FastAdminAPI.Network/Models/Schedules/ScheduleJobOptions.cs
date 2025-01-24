18246116639cd8cbd1b3ca3310f188359ed01e97
Willxup <wangxking@outlook.com>
2025-01-24 18:39:09 +0800

    update appsettings.json file

---
+++ FastAdminAPI.Network/Models/Schedules/ScheduleJobOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace FastAdminAPI.Network.Models.Schedules
+{
+    public class ScheduleJobOptions
+    {
+        public bool IsEnable { get; set; }
+        public string JobName { get; set; }
+        public string Frequency { get; set; }
+        public string Minute { get; set; }
+        public string Hour { get; set; }
+        public string Day { get; set; }
+        public string DayOfWeek { get; set; }
+        public string Month { get; set; }
+        public string Description { get; set; }
+    }
+}
